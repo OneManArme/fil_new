e543488e9e64719fd8c5e23720b33139edf3eb71
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2025-01-31 00:51:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1554,7 +1554,7 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
     else:
         #cap = f"""<b>📂 ʜᴇʀᴇ ɪ ꜰᴏᴜɴᴅ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ {search}\n<a href='https://t.me/+f5wP20a_ah44Mjdl'>Another Group Join</a></b>"""
 	cap = f"<b>📂 ʜᴇʀᴇ ɪ ꜰᴏᴜɴᴅ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ {search}</b>"
-	button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Another Group", url="https://t.me/+f5wP20a_ah44Mjdl")]])
+	    button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Another Group", url="https://t.me/+f5wP20a_ah44Mjdl")]])
 
 await message.reply_text(cap, reply_markup=button)
 
