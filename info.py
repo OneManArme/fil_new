99cdd302e888066807c02b38fec73f9c25b40b09
OneManArme <asuran1manarmy@gmail.com>
2025-06-20 21:02:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,9 +43,9 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link 
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/demoshort/")
-TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/demoshort/")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/demoshort/")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/demoshort/70")
+TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/demoshort/70")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/demoshort/70")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API2 = environ.get("SHORTENER_API2", "10c45cd3943f38044756d4822d8691efd634d174")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'krownlinks.com')
